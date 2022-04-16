f43ee642f6d9e45662f36e5ac2455db1db406222
javierandresgp <info@javierandres.dev>
2022-04-15 21:28:57 -0500

    training

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const productRouter = require('./routes/products');
 
 const server = express();
 const port = 4000;
 
-server.listen(port, () => console.log(`Server running on port ${port}`));
-
-server.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+server.use(cors());
+server.use(express.json());
+server.use('/api', productRouter);
 
 dotenv.config();
 
@@ -19,5 +18,8 @@ mongoose
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(`Error: ${err}`));
 
-server.use(express.json());
-server.use('/api', productRouter);
+server.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Título';
-  subtitle = 'subtítulo';
+  title = 'Filtering Facepiece Respirator (FFR)';
+  subtitle = 'Products';
+  product = {
+    name: '',
+    price: 0,
+    imgUrl: '',
+  };
+  products = <any>[];
+
+  ngOnInit() {
+    this.readAll();
+  }
+
+  createProduct() {
+    fetch('http://localhost:4000/api/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.product),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log('Success:', json);
+        this.product = {
+          name: '',
+          price: 0,
+          imgUrl: '',
+        };
+      })
+      .catch((err) => console.log('Error:', err));
+  }
+
+  readAll() {
+    fetch('http://localhost:4000/api/products')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log('Success:', json);
+        this.products = json.success;
+        console.log('products:', this.products);
+      })
+      .catch((err) => console.log('Error:', err));
+  }
 }
